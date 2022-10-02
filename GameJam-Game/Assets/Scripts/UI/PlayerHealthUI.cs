173f834cb7401b23406fe937c0aef2f2b20589b3
davidweimer <david-weimer@web.de>
2022-10-02 15:22:31 +0200

    Health bar animation (Hit animation and low health animantion).

--- GameJam-Game/Assets/Scripts/UI/PlayerHealthUI.cs
+++ GameJam-Game/Assets/Scripts/UI/PlayerHealthUI.cs
@@ -3,6 +3,8 @@ using Nidavellir.EventArgs;
 using Nidavellir.Scriptables;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Nidavellir.UI
 {
@@ -10,6 +12,8 @@ namespace Nidavellir.UI
     {
         [SerializeField] private Resource m_playerHealthResource;
         [SerializeField] private TextMeshProUGUI m_healthText;
+        [SerializeField] private float m_currentHealth;
+        [SerializeField] private Image m_background;
 
         private void Awake()
         {
@@ -19,11 +23,41 @@ namespace Nidavellir.UI
         private void Start()
         {
             this.m_healthText.text = this.m_playerHealthResource.ResourceController.CurrentValue.ToString();
+            m_currentHealth = this.m_playerHealthResource.ResourceController.CurrentValue;
+        }
+
+        private void Update()
+        {
+            if (m_currentHealth <= 8)
+            {
+                Debug.Log("Low on health!");
+                AnimateHealthBarLow();
+            }
         }
 
         private void PlayerHealthChanged(object sender, ResourceValueChangedEventArgs e)
         {
             this.m_healthText.text = e.NewValue.ToString();
+            this.m_currentHealth = e.NewValue;
+            Debug.Log("m_currentHealth:" + m_currentHealth);
+            AnimateHealthBarHit();
         }
+
+        private void AnimateHealthBarHit()
+        {
+             this.transform.DOShakePosition(0.5f, 5);
+             this.transform.DOShakeScale(0.5f, 0.1f, randomnessMode:ShakeRandomnessMode.Harmonic);
+             // this.m_background.DOColor(Color.red, 0.25f).SetLoops(2, LoopType.Yoyo);
+             this.m_healthText.DOColor(Color.red, 0.25f).SetLoops(2, LoopType.Yoyo);
+        }
+
+        private void AnimateHealthBarLow()
+        {
+            this.transform.DOShakePosition(-1, 6);
+            this.transform.DOShakeScale(0.5f, 0.1f, vibrato:5, randomnessMode:ShakeRandomnessMode.Harmonic);
+            this.m_healthText.DOColor(Color.red, 0.25f);
+        }
+
+
     }
 }
