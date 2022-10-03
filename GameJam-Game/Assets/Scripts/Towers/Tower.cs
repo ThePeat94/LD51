c3bf56d15cad9c5c90df5be51acbcd4cc8eaad88
Seref Bülbül <bulbulseref@gmail.com>
2022-10-03 13:36:00 +0200

    Improve tower placement mechanism

--- GameJam-Game/Assets/Scripts/Towers/Tower.cs
+++ GameJam-Game/Assets/Scripts/Towers/Tower.cs
@@ -1,10 +1,9 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Nidavellir.Scriptables;
 using Nidavellir.Towers.Projectiles;
 using Nidavellir.Trigger;
-using Unity.VisualScripting;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -12,14 +11,23 @@ namespace Nidavellir.Towers
 {
     public class Tower : MonoBehaviour
     {
-        [Header("References")] 
+        [Header("References")]
         public TowerSO TowerSettings;
         public SphereBaseTrigger AttackBaseTrigger;
         public Transform ProjectileSpawnPoint;
         public Transform Muzzle;
-        
+
         [SerializeField] private GameObject rangeIndicator;
-        
+        [SerializeField] private MeshRenderer rangeIndicatorMeshRenderer;
+
+        [ColorUsageAttribute(true, true)] [SerializeField]
+        private Color rangeIndicatorColor;
+
+        [ColorUsageAttribute(true, true)] [SerializeField]
+        private Color nonPlaceableRangeIndicatorColor;
+
+        [SerializeField] private BoxCollider boxCollider;
+
         private float timeUntilNextAttack;
         private GameObject currentTarget;
         private List<GameObject> enemiesInRange = new List<GameObject>();
@@ -27,7 +35,7 @@ namespace Nidavellir.Towers
         private List<TowerUpgradeSO> appliedUpgrades = new List<TowerUpgradeSO>();
         private GameObject closestEnemy = null;
         private Queue<TowerUpgradeSO> m_towerUpgrades;
-        
+
         public float TowerRange { get; protected set; }
         public float AttackSpeed { get; protected set; }
         public float Damage { get; protected set; }
@@ -35,11 +43,17 @@ namespace Nidavellir.Towers
         public int CurrentLevel { get; private set; }
         public float CostsForNextLevel => this.m_towerUpgrades.Count > 0 ? this.m_towerUpgrades.Peek().Price : 0;
         public bool IsPlaced => this.isPlaced;
-        
+
+        private static readonly int Emission = Shader.PropertyToID("_Emission");
+
         private void Start()
         {
             var rangeCofactor = 2 * TowerSettings.TowerRange;
-            rangeIndicator.transform.localScale = new Vector3(rangeCofactor, rangeCofactor, rangeCofactor) ;
+            rangeIndicator.transform.localScale = new Vector3(rangeCofactor, rangeCofactor, rangeCofactor);
+
+            // var intensity = 2;
+            // float factor = Mathf.pow(2,intensity);
+            // Color color = new Color(red*factor,green*factor,bleue*factor);
         }
 
         [ContextMenu("Place")]
@@ -48,33 +62,33 @@ namespace Nidavellir.Towers
             Init();
             Place();
         }
-        
+
         public void Init()
         {
             TowerRange = TowerSettings.TowerRange;
             AttackSpeed = TowerSettings.AttackSpeed;
             Damage = TowerSettings.Damage;
             Projectile = TowerSettings.Projectile;
-            
+
             AttackBaseTrigger.Init(Vector3.one * TowerSettings.TowerRange);
             AttackBaseTrigger.EventOnTriggerEnter += AddEnemyInRange;
             AttackBaseTrigger.EventOnTriggerExit += RemoveEnemyInRange;
-            
+
             timeUntilNextAttack = 0;
         }
 
         private void Update()
         {
-            if(!isPlaced)
+            if (!isPlaced)
                 return;
 
             closestEnemy = GetClosestEnemy();
-            
+
             if (Muzzle != null && closestEnemy != null)
             {
                 Muzzle.LookAt(new Vector3(closestEnemy.transform.position.x, closestEnemy.transform.position.y, closestEnemy.transform.position.z));
             }
-            
+
             Shoot(Time.deltaTime);
         }
 
@@ -86,7 +100,7 @@ namespace Nidavellir.Towers
             {
                 if (closestEnemy == null)
                     return;
-                
+
                 var projectile = Object.Instantiate(Projectile.gameObject).GetComponent<Projectile>();
 
                 if (ProjectileSpawnPoint != null)
@@ -95,22 +109,22 @@ namespace Nidavellir.Towers
                 }
                 else
                 {
-                    Debug.LogError($"{ TowerSettings.Name }: no ProjectileSpawnPoint defined");
+                    Debug.LogError($"{TowerSettings.Name}: no ProjectileSpawnPoint defined");
                     projectile.transform.position = transform.position;
                 }
-                
+
                 projectile.Init(closestEnemy, closestEnemy.transform.position, Damage);
 
                 timeUntilNextAttack = AttackSpeed;
             }
         }
-        
+
         private GameObject GetClosestEnemy()
         {
             var enemiesHitByOverlapSphere = Physics.OverlapSphere(this.transform.position, this.TowerRange)
                 .Where(c => c.TryGetComponent<EnemyHealthController>(out _)).ToArray();
-            
-            if(enemiesHitByOverlapSphere.Length > 0)
+
+            if (enemiesHitByOverlapSphere.Length > 0)
                 return enemiesHitByOverlapSphere.OrderBy(x => Vector3.Distance(transform.position, x.transform.position)).First().gameObject;
 
             return null;
@@ -120,29 +134,43 @@ namespace Nidavellir.Towers
         {
             enemiesInRange.Add(enemy);
         }
-        
+
         private void RemoveEnemyInRange(GameObject enemy)
         {
             enemiesInRange.Remove(enemy);
+        }
+
+        public void SetTowerPlaceable(bool placeable)
+        {
+            rangeIndicatorMeshRenderer.material.SetColor(Emission, placeable ? rangeIndicatorColor : nonPlaceableRangeIndicatorColor);
         }
 
         public bool Place()
         {
             if (CurrencyController.Instance.BuyItem(TowerSettings.Price))
             {
-                isPlaced = true;
-                SetRangeIndicatorActive(false);
-                this.CurrentLevel++;
-                this.m_towerUpgrades = new(this.TowerSettings.PossibleUpgrades);
+                StartCoroutine(PlaceTower());
                 return true;
             }
-            
+
             return false;
+        }
+
+        private IEnumerator PlaceTower()
+        {
+            yield return null;
+
+            isPlaced = true;
+            boxCollider.enabled = true;
+            SetRangeIndicatorActive(false);
+            this.CurrentLevel++;
+            this.m_towerUpgrades = new(this.TowerSettings.PossibleUpgrades);
         }
 
         public void Unplace()
         {
             isPlaced = false;
+            boxCollider.enabled = false;
             SetRangeIndicatorActive(true);
         }
 
@@ -182,12 +210,13 @@ namespace Nidavellir.Towers
 
             this.CurrentLevel++;
         }
-        
-        
+
+
 #if UNITY_EDITOR
         private void OnValidate()
         {
             AttackBaseTrigger?.SetSize(Vector3.one * TowerSettings.TowerRange);
+            rangeIndicatorMeshRenderer.sharedMaterial.SetColor(Emission, rangeIndicatorColor);
         }
 
         private void OnDrawGizmos()
--- GameJam-Game/Assets/Scripts/UI/TowersPanelPresenter.cs
+++ GameJam-Game/Assets/Scripts/UI/TowersPanelPresenter.cs
@@ -21,8 +21,6 @@ namespace Nidavellir.UI
         private Camera mainCamera;
         private InputAction click;
         private Tower activeTower;
-        private MeshRenderer activeTowerMeshRenderer;
-        private Color activeTowerOriginalColor;
         private TowerButton selectedTowerButton;
 
         private void Awake()
@@ -39,6 +37,7 @@ namespace Nidavellir.UI
                 towerButton.SetTowerSo(towerSo);
                 towerButton.OnButtonClick = towerSo =>
                 {
+                    Debug.Log("Tower button clicked");
                     if (activeTower != null)
                     {
                         Destroy(activeTower.gameObject);
@@ -50,16 +49,12 @@ namespace Nidavellir.UI
                         {
                             var activeTowerObject = Instantiate(towerSo.TowerPrefab.gameObject);
                             activeTower = activeTowerObject.GetComponent<Tower>();
-                            activeTowerMeshRenderer = activeTowerObject.GetComponentInChildren<MeshRenderer>();
-                            activeTowerOriginalColor = activeTowerMeshRenderer.material.color;
                         }
                     }
                     else
                     {
                         var activeTowerObject = Instantiate(towerSo.TowerPrefab.gameObject);
                         activeTower = activeTowerObject.GetComponent<Tower>();
-                        activeTowerMeshRenderer = activeTowerObject.GetComponentInChildren<MeshRenderer>();
-                        activeTowerOriginalColor = activeTowerMeshRenderer.material.color;
                     }
                 };
             }
@@ -72,21 +67,20 @@ namespace Nidavellir.UI
                 var mousePosition = Mouse.current.position.ReadValue();
                 var ray = mainCamera.ScreenPointToRay(mousePosition);
 
-                // var nonPlaceableAreaLayerMask = ~placeableAreaLayerMask;
-
-                if (Physics.Raycast(ray, out var raycastHit, float.MaxValue, withoutAttackTriggerLayerMask) &&
+                if (Physics.Raycast(ray, out var _, float.MaxValue, withoutAttackTriggerLayerMask) &&
                     Physics.Raycast(ray, out var raycastGroundHit, float.MaxValue, placeableAreaLayerMask))
                 {
                     var mouseWorldPosition = raycastGroundHit.point;
                     mouseWorldPosition.y = 0.5f;
                     activeTower.transform.position = mouseWorldPosition;
-                    
+
                     var castAll = Physics.BoxCastAll(mouseWorldPosition, Vector3.one, Vector3.up, Quaternion.identity, float.MaxValue, nonPlaceableAreaLayerMask);
                     if (castAll.Length == 0) // it shouldn't hit anything except ground
                     {
-                        activeTowerMeshRenderer.material.color = activeTowerOriginalColor;
+                        activeTower.SetTowerPlaceable(true);
+                        // activeTowerMeshRenderer.material.color = activeTowerOriginalColor;
 
-                        if (Mouse.current.rightButton.wasPressedThisFrame && activeTower.Place())
+                        if (Mouse.current.leftButton.wasPressedThisFrame && activeTower.Place())
                         {
                             activeTower.Init();
                             activeTower = null;
@@ -94,12 +88,12 @@ namespace Nidavellir.UI
                     }
                     else
                     {
-                        activeTowerMeshRenderer.material.color = Color.black;
+                        activeTower.SetTowerPlaceable(false);
                     }
                 }
                 else
                 {
-                    activeTowerMeshRenderer.material.color = Color.black;
+                    activeTower.SetTowerPlaceable(false);
                 }
             }
         }
